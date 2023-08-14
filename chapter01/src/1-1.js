120fb775a349a858c130e4a8f198c2ab6a971a9c
Marcus <814712074@qq.com>
2023-08-14 11:13:07 +0800

    一些类型

--- chapter01/src/1-1.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// 类型注解
-let data = 1;
-// 类型推断
-let money = 1;
---
+++ chapter01/src/1-3.ts
@@ -0,0 +1,20 @@
+// 根类型
+let data: Object = 1; // 除了 null undefined 其他基本能赋值
+let data2: {} = 2; // {} 等同 Object
+
+// 对象类型 object Array function
+let o1: object = {};
+
+// 合成类型
+// 联合类型
+let str: string | number = 1;
+// 交叉类型
+type O1 = { name: string };
+type O2 = { age: number };
+let o2: O1 & O2 = { name: "1", age: 1 };
+
+// 字面量数据类型
+type Num = 1 | 2;
+let num: Num = 2;
+
+export {};
