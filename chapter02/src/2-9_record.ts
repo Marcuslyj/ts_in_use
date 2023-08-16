0e393903c12734299d20123bac1711dd91ba0090
Marcus <814712074@qq.com>
2023-08-16 14:21:20 +0800

    record

--- chapter02/src/2-9_record.ts
+++ chapter02/src/2-9_record.ts
@@ -10,4 +10,10 @@ let r1: R1 = { name: "1", age: 1 };
 type R2 = Record<string, string | number>;
 let r2: R2 = { name: "1" };
 
+type R = Record<string, any>;
+let r: R = [1];
+let r3: R = new Set();
+let r4: R = new Map();
+let r5: R = function () {};
+
 export {};
