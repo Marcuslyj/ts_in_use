43fcf60829f424e1e1495fee6104f2ca36a78a74
Marcus <814712074@qq.com>
2023-08-16 15:59:04 +0800

    capitalize

---
+++ chapter02/src/2-12_capitalize.ts
@@ -0,0 +1,16 @@
+interface Todo {
+  title: string;
+  completed: boolean;
+  add(): number;
+  del(): number;
+}
+
+// 重映射
+type Degree<T> = {
+  [P in keyof T as T[P] extends Function
+    ? `do${Capitalize<P & string>}`
+    : never]: T[P];
+};
+
+type TestDegree = Degree<Todo>;
+export {};
